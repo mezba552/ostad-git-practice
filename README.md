8b511eb6ada28b54dcd29f3cf1a9c821e1a025be
mezba552 <mezbaahmed86@gmail.com>
2026-06-05 17:42:37 +0600

    imported necessary library

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning Version Control
+
+# Hello
--- app.py
+++ app.py
@@ -2,3 +2,6 @@ print('Hello, World!')
 print('Version control')
 
 print('This is a new line added to the code.')
+
+import math
+import random
